319b58ea1d8626a9d1a7e0c07aed2451ac99d6f6
Gokuktkn <120592109+Gokuktkn@users.noreply.github.com>
2024-06-05 02:47:00 +0700

    UI

--- Frontend/vite-project/src/components/MovieDetails.jsx
+++ Frontend/vite-project/src/components/MovieDetails.jsx
@@ -3,12 +3,19 @@ import React from 'react';
 function MovieDetails({ movie, onClose }) {
   return (
     <div className="movie-details">
-      <button className="close" onClick={onClose}>X</button>
-      <img src={movie.image} alt={movie.name} />
+      <img src={movie.image} alt={movie.name} className='movie-detail-img' />
       <div className="movie-info">
-        <h2>{movie.name}</h2>
-        <p>{movie.time} min {movie.year}</p>
+        <div className="movie-name">
+          <h2>{movie.name}</h2>
+          <p>{movie.time} min {movie.year}</p>
+        </div>
         <p>{movie.introduce}</p>
+        <div className="play-btn-div">
+          <button className='play-btn'>PLAY MOVIE</button>
+        </div>
+        <div className="close-btn-div">
+          <button className="close-btn" onClick={onClose}>X</button>
+        </div>
       </div>
     </div>
   );
--- Frontend/vite-project/src/components/MovieList.jsx
+++ Frontend/vite-project/src/components/MovieList.jsx
@@ -1,11 +1,26 @@
-function MovieList({ movies, onMovieClick }) {
+import { useState } from "react";
+
+const MovieList = ({ movies, onMovieClick }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const moviesPerPage = 4;
+
+  const indexOfLastMovie = currentPage * moviesPerPage;
+  const indexOfFirstMovie = indexOfLastMovie - moviesPerPage;
+  const currentMovies = movies.slice(indexOfFirstMovie, indexOfLastMovie);
+
+  const handleNextPage = () => {
+    setCurrentPage(currentPage + 1);
+  };
+  const handlePreviousPage = () => {
+    setCurrentPage(currentPage - 1);
+  };
   return (
     <div className="movie-list">
       <h2>Most Popular Movies</h2>
       <div className="movies">
-        {movies.map((movie) => (
+        {currentMovies.map((movie) => (
           <div key={movie.ID} className="movie" onClick={() => onMovieClick(movie)}>
-            <img src={movie.image} alt={movie.name} />
+            <img src={movie.image} alt={movie.name} className="movie-img" />
             <div className="movie-info">
               <h3>{movie.name}</h3>
               <p>{movie.time} min {movie.year}</p>
@@ -13,7 +28,12 @@ function MovieList({ movies, onMovieClick }) {
           </div>
         ))}
       </div>
-      {movies.length > 4 && <button className="next-button">Next</button>}
+      <div className="nav-btn">
+        {currentPage > 1 && (
+          <button onClick={handlePreviousPage}>Previous</button>
+        )}
+        {movies.length > 4 && <button onClick={handleNextPage}>Next</button>}
+      </div>
     </div>
   );
 }
